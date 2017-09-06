7644d4238eae852166cc57aad0ebf21cfbbf2e0e
valeriy-novytskyy <valeriy.novytskyy@outlook.com>
2017-09-05 21:38:53 -0700

    Setup mocha tests with chai and chai jsx

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import './style';
 import App from './components/App';
 import withState from './state';
 
-if (typeof withState !== 'function') {
-	console.log('withState is not a func');
-}
-
 export default withState(App);
--- test/lib/getDefaultPresetName.tests.js
+++ test/lib/getDefaultPresetName.tests.js
@@ -1,32 +1,36 @@
+const expect = require('chai').expect;
 const getDefaultPresetName = require('../../src/lib/getDefaultPresetName');
 
-test('returns the first sub-object with default property set', () => {
-	expect(getDefaultPresetName({
-		// has default false
-		one: {
-			default: false
-		},
-		// has default true (should be chosen)
-		two: {
-			default: true
-		},
-		// has default undefined
-		three: {
-		}
-	})).toBe('two');
-});
+describe('getDefaultPresetName', () => {
 
-test('returns undefined if a default preset could not be found', () => {
-	expect(getDefaultPresetName({
-		// has default false
-		one: {
-			default: false
-		},
-		// has default true (should be chosen)
-		two: {
-		},
-		// has default undefined
-		three: {
-		}
-	})).toBe(undefined);
+	it('returns the first sub-object with default property set', () => {
+		expect(getDefaultPresetName({
+			// has default false
+			one: {
+				default: false
+			},
+			// has default true (should be chosen)
+			two: {
+				default: true
+			},
+			// has default undefined
+			three: {
+			}
+		})).to.equal('two');
+	});
+	
+	it('returns undefined if a default preset could not be found', () => {
+		expect(getDefaultPresetName({
+			// has default false
+			one: {
+				default: false
+			},
+			// has default true (should be chosen)
+			two: {
+			},
+			// has default undefined
+			three: {
+			}
+		})).to.equal(undefined);
+	});
 });
