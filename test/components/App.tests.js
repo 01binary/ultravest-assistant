31412a987dd9923bdb018ed2e10509c68cae9c13
Valeriy Novytskyy <vnovytskyy@webmd.net>
2017-09-08 10:27:19 -0700

    Complete App tests

--- test/components/App.tests.js
+++ test/components/App.tests.js
@@ -1,16 +1,25 @@
-
 import { expect } from 'chai';
-import render from '../fixtures/component.tests.fixture';
-import withState from '../../src/composers';
 import App from '../../src/components/App';
+import Flask from '../../src/components/Flask';
+import Investment from '../../src/components/Investment';
+import Burnout from '../../src/components/Burnout';
+import Summary from '../../src/components/Summary';
+import Result from '../../src/components/Result';
+import withState from '../../src/composers';
+import {
+	wrap,
+	expectTag,
+	expectParentTag,
+	expectAttribute,
+	findComponent
+} from '../fixtures/component.tests.fixture';
 
 describe('App', () => {
 
 	let wrapper;
 
 	before(() => {
-		wrapper = render(withState(App));
-		console.log(wrapper);
+		wrapper = wrap(withState(App));
 	});
 
 	after(() => {
@@ -18,22 +27,49 @@ describe('App', () => {
 	});
 
 	it('should render wrapper', () => {
-		expect(wrapper.nodeName).to.equal('DIV',
-			'should render a div');
-		expect(wrapper.attributes[0]).to.eql({
-			ns: null,
-			name: 'id',
-			value: 'app'
-		}, 'should set div id to app');
+		expectTag(wrapper, 'div', 'should render div');
+		expectAttribute(wrapper, 'id', 'app', 'should have id "app"');
 	});
 
-	it('should render Flask');
+	it('should render Flask', () => {
+		const flask = findComponent(wrapper, Flask);
+		expect(flask).to.have.length(1, 'should render a single Flask component');
+		expectParentTag(flask, 'main', 'should render Flask inside main');
+	});
 
-	it('should render Investment');
+	it('should render Investment', () => {
+		const investment = findComponent(wrapper, Investment);
 
-	it('should render Burnout');
+		expect(investment).to.have.length(1,
+			'should render a single Investment component');
+		expectParentTag(investment, 'main',
+			'should render Investment inside main');
+	});
 
-	it('should render Result');
+	it('should render Burnout', () => {
+		const burnout = findComponent(wrapper, Burnout);
 
-	it('should render Summary');
+		expect(burnout).to.have.length(1,
+			'should render a single Burnout component');
+		expectParentTag(burnout, 'main',
+			'should render Burnout inside main');
+	});
+
+	it('should render Result', () => {
+		const result = findComponent(wrapper, Result);
+
+		expect(result).to.have.length(1,
+			'should render a single Result component');
+		expectParentTag(result, 'main',
+			'should render Result inside main');
+	});
+
+	it('should render Summary', () => {
+		const summary = findComponent(wrapper, Summary);
+
+		expect(summary).to.have.length(1,
+			'should render a single Summary component');
+		expectParentTag(summary, 'aside',
+			'should render Summary in aside');
+	});
 });
--- test/fixtures/component.tests.fixture.js
+++ test/fixtures/component.tests.fixture.js
@@ -1,10 +1,69 @@
 import { h, render } from 'preact';
+import { expect } from 'chai';
 
 /**
  * Renders an instance of the specified component.
  * @param {function} componentFactory - The component to render.
  * @returns {object} - The rendered Dom tree.
  */
-export default (componentFactory) => (
-	render(h(componentFactory))
-);
+export function wrap(componentFactory) {
+	return render(h(componentFactory));
+}
+
+export function expectChild(wrapper, nodeName, message) {
+	expect(findNode(wrapper, nodeName))
+		.to.have.length(1, message);
+}
+
+export function expectComponent(wrapper, component, message) {
+	expect(findComponent(wrapper, component))
+		.to.have.length(1, message);
+}
+
+export function expectTag(wrapper, tagName, message) {
+	expect(wrapper.nodeName.toLowerCase() === tagName)
+		.to.equal(true, message);
+}
+
+export function expectParentTag(elements, tagName, message) {
+	const parentHasTag = expectParentTagSingle(tagName, message);
+
+	Array.isArray(elements) ?
+		elements.forEach(element => parentHasTag) :
+		parentHasTag(elements);
+}
+
+const expectParentTagSingle = (tagName, message) => (element) =>
+	expect(element.parentNode.nodeName.toLowerCase() === tagName)
+		.to.equal(true, message);
+
+export function expectAttribute(wrapper, name, value, message) {
+	expect(wrapper.attributes.filter(attribute =>
+		attribute.name.toLowerCase() === name &&
+		attribute.value === value
+	)).to.have.length(1, message);
+}
+
+export function findNode(parent, nodeName) {
+	let matches = parent.childNodes.filter(
+		node => node.nodeName.toLowerCase() === nodeName
+	);
+
+	parent.childNodes.forEach(
+		child => findNode(child, nodeName).map(found => matches.push(found))
+	);
+
+	return matches;
+}
+
+export function findComponent(parent, component) {
+	let matches = parent.childNodes.filter(
+		node => node._componentConstructor === component
+	);
+
+	parent.childNodes.forEach(
+		child => findComponent(child, component).map(found => matches.push(found))
+	);
+
+	return matches;
+}
