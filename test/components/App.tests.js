c1c92aab61c8ac6d9d37d98bb06852ae9a9c09e1
01binary <valeriy.novytskyy@outlook.com>
2017-09-08 08:34:17 -0700

    Attempt Dom tests

--- test/components/App.tests.js
+++ test/components/App.tests.js
@@ -1,11 +1,28 @@
-import { h } from 'preact';
+import { h, render } from 'preact';
 import { expect } from 'chai';
+import withState from '../../src/composers';
 import App from '../../src/components/App';
 
 describe('App', () => {
 
+	let wrapper;
+
+	before(() => {
+		wrapper = render(withState(<App />), global.document);
+		console.log(wrapper);
+	});
+
+	after(() => {
+		wrapper = null;
+	});
+
 	it('should render wrapper', () => {
-		expect(<App />).to.contain(<div id="app" />);
+		
+		expect(wrapper.nodeName).to.equal('DIV');
+		expect(wrapper.attributes).to.contain({
+			name: 'id',
+			value: 'app'
+		});
 	});
 
 	it('should render Flask');
