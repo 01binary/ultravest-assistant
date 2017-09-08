8eaa55820b370a63f8c2adc89305faa305936f48
valeriy-novytskyy <valeriy.novytskyy@outlook.com>
2017-09-07 23:35:18 -0700

    Starting App tests

---
+++ test/components/App.tests.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+
+describe('App', () => {
+
+	it('should render wrapper', () => {
+		
+	});
+
+	it('should render Flask');
+
+	it('should render Investment');
+
+	it('should render Burnout');
+
+	it('should render Result');
+
+	it('should render Summary');
+});
