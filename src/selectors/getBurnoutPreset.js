b6dbe6246acd6b3b2624154c3a40046724247a0c
01binary <valeriy.novytskyy@outlook.com>
2017-09-12 13:40:23 -0700

    Refactoring burnout preset to fix coverage

--- src/selectors/getBurnoutPreset.js
+++ src/selectors/getBurnoutPreset.js
@@ -1,15 +1,26 @@
-const byFlaskSizeAscending = (preset1, preset2) => (
-	(
-		preset1.diameter > preset2.diameter &&
-		preset1.height > preset2.height
-	) ? 1 : -1
-);
-
+/**
+ * Get burnout preset for flask size.
+ * @param {object} burnoutPresets - The available burnout presets.
+ * @param {object} flask - The flask size.
+ * @returns - The shortest burnout for the given flask or the longest, if flask is too big.
+ */
 export default (burnoutPresets, flask) => (
 	burnoutPresets
-		.sort(byFlaskSizeAscending)
-		.filter((preset) => (
+		.sort(flaskSizeAscending)
+		.filter(preset => (
 			preset.diameter >= flask.diameter &&
 			preset.height >= flask.height
-		)) [0] || burnoutPresets[burnoutPresets.length - 1]
+		))
+		.reduce(lastOrDefault)
 );
+
+function flaskSizeAscending(first, second) {
+	return (
+		first.diameter > second.diameter &&
+		first.height > second.height
+	) ? 1 : -1;
+}
+
+function lastOrDefault(acc, preset) {
+	return preset;
+}
