45f9986cda70c55b6d804776d285e280c98c2fbc
01binary <valeriy.novytskyy@outlook.com>
2017-09-15 13:11:07 -0700

    Responsive styles in progress

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,17 +1,12 @@
 
 import { h } from 'preact';
-import getDefaultPresetName from '../../selectors/getDefaultPresetName';
-import flaskPresets from '../../config/flaskPresets';
-import investmentPresets from '../../config/investmentPresets';
 
-const Result = ({ flask, investment }) =>
-
-(
+const Result = ({ flask, investment }) => (
 	<p>
 		Enter your flask size and the investment-to-water ratio to see how much investment and water are required.
 		The kiln program is calculated automatically.
 
-		<input type="submit" />
+		<input type="submit" value="Calculate" />
 	</p>
 );
 
