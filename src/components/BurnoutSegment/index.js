745ac7f1b2d36ac03608a7854367d65b26c0ce56
01binary <valeriy.novytskyy@outlook.com>
2017-09-29 13:08:24 -0700

    #5 Starting burnout diagram

---
+++ src/components/BurnoutSegment/index.js
@@ -0,0 +1,14 @@
+import { h } from 'preact';
+import style from './style';
+
+/**
+ * Burnout diagram secment.
+ * @param {*} props 
+ */
+const BurnoutSegment = ({  }) => (
+	<article>
+
+	</article>
+);
+
+export default BurnoutSegment;
