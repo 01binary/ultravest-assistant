f0131247fda771351a1f03d847be6d823095c5ef
01binary <valeriy.novytskyy@outlook.com>
2017-09-29 12:24:45 -0700

    #5 getBurnoutSteps selector

--- src/selectors/getBurnoutSteps.js
+++ src/selectors/getBurnoutSteps.js
@@ -2,12 +2,10 @@
  * Get friendly steps from a machine readable burnout program.
  * @param {object} preset - The burnout preset.
  * @returns {object[]} - The burnout steps ready for formatted output.
- * TODO: diagram for naming
  */
 export default preset => preset.schedule.reduce(
 	(steps, segment, index, schedule) => {
 		const lastStep = steps.length && steps[steps.length - 1];
-		const formattedTemp = `${segment.temp}°F`;
 		const diff = lastStep ? segment.temp - lastStep.temp : segment.temp;
 		const duration = Math.round(Math.abs(diff / segment.rate) * 10) / 10;
 
@@ -18,7 +16,7 @@ export default preset => preset.schedule.reduce(
 			steps.push({
 				heading: segment.name,
 				action: diff < 0 ? 'lower to' : 'raise to',
-				temp: formattedTemp,
+				temp: segment.temp,
 				middle: 'over',
 				time: `${duration} ${getUnits(duration)}`,
 				end: index === schedule.length - 1 ? 'for casting' : null
@@ -31,7 +29,7 @@ export default preset => preset.schedule.reduce(
 			steps.push({
 				heading: segment.name,
 				action: 'hold',
-				temp: formattedTemp,
+				temp: segment.temp,
 				middle: 'for',
 				time: `${segment.hold} ${getUnits(segment.hold)}`
 			});
--- test/selectors/getBurnoutSteps.tests.js
+++ test/selectors/getBurnoutSteps.tests.js
@@ -22,7 +22,7 @@ describe('selector getBurnoutSteps', () => {
 			{
 				heading: 'test heading',
 				action: 'raise to',
-				temp: '500°F',
+				temp: 500,
 				middle: 'over',
 				time: '2 hours',
 				end: 'for casting'
@@ -30,22 +30,117 @@ describe('selector getBurnoutSteps', () => {
 			{
 				heading: 'test heading',
 				action: 'hold',
-				temp: '500°F',
+				temp: 500,
 				middle: 'for',
 				time: '1 hour'
 			}
 		]);
 	});
 
-	test('calculates down transition and hold step for one segment', () => {
-	});
-
 	test('calculates up transition with no hold for one segment', () => {
+		expect(getBurnoutSteps({
+			schedule: [
+				{
+					name: 'test heading',
+					rate: 250,
+					temp: 500,
+					hold: 0
+				}
+			]
+		})).toEqual([
+			{
+				heading: 'test heading',
+				action: 'raise to',
+				temp: 500,
+				middle: 'over',
+				time: '2 hours',
+				end: 'for casting'
+			}
+		]);
 	});
 
-	test('calculates only hold for one segment', () => {
+	test('calculates only hold for segments with no difference', () => {
+		expect(getBurnoutSteps({
+			schedule: [
+				{
+					name: 'test heading',
+					rate: 250,
+					temp: 500,
+					hold: 0
+				},
+				{
+					name: 'test heading',
+					rate: 250,
+					temp: 500,
+					hold: 1
+				}
+			]
+		})).toEqual([
+			{
+				heading: 'test heading',
+				action: 'raise to',
+				temp: 500,
+				middle: 'over',
+				time: '2 hours',
+				end: null
+			},
+			{
+				heading: 'test heading',
+				action: 'hold',
+				temp: 500,
+				middle: 'for',
+				time: '1 hour'
+			}
+		]);
 	});
 
 	test('calculates four steps with two segments', () => {
+		expect(getBurnoutSteps({
+			schedule: [
+				{
+					name: 'first segment',
+					rate: 300,
+					temp: 600,
+					hold: 1
+				},
+				{
+					name: 'second segment',
+					rate: 50,
+					temp: 700,
+					hold: 5
+				}
+			]
+		})).toEqual([
+			{
+				heading: 'first segment',
+				action: 'raise to',
+				temp: 600,
+				middle: 'over',
+				time: '2 hours',
+				end: null
+			},
+			{
+				heading: 'first segment',
+				action: 'hold',
+				temp: 600,
+				middle: 'for',
+				time: '1 hour'
+			},
+			{
+				heading: 'second segment',
+				action: 'raise to',
+				temp: 700,
+				middle: 'over',
+				time: '2 hours',
+				end: 'for casting'
+			},
+			{
+				heading: 'second segment',
+				action: 'hold',
+				temp: 700,
+				middle: 'for',
+				time: '5 hours'
+			}
+		]);
 	});
 });
