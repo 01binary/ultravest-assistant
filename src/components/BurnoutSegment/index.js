5e40798387ac6923e42a571e7f4b787072c2e07c
01binary <valeriy.novytskyy@outlook.com>
2017-09-29 20:00:17 -0700

    #5 Starting diagram segment

--- src/components/BurnoutSegment/index.js
+++ src/components/BurnoutSegment/index.js
@@ -2,13 +2,35 @@ import { h } from 'preact';
 import style from './style';
 
 /**
- * Burnout diagram secment.
- * @param {*} props 
+ * Burnout diagram segment.
+ * @param {Object} prev - The previous segment.
+ * @param {Object} cur - The current segment.
  */
-const BurnoutSegment = ({  }) => (
-	<article>
+const BurnoutSegment = ({ prev, cur }) => (
+	<section class={[
+		style.segment,
+		cur.temp > prev.temp ?
+			style.segmentRaise :
+			style.segmentLower]}
+	>
+		<article class={style.temperature}>
+			<h3>temp</h3>
+			{ prev ? 'ambient' : `${cur.temp}°` }
+			<span class={style.units}>F</span>
+		</article>
 
-	</article>
+		<article class={style.rate}>
+			<h3>rate</h3>
+			{ `${cur.rate}°` }
+			<span class={style.units}>F/hr</span>
+		</article>
+
+		<article class={style.time}>
+			<h3>hold</h3>
+			{ cur.hold }
+			<span class={style.units}>hours</span>
+		</article>
+	</section>
 );
 
 export default BurnoutSegment;
