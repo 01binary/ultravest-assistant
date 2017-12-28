401882bfcd8dee9a92ce4c79dd41e6d433b30855
01binary <valeriy.novytskyy@outlook.com>
2017-12-27 19:13:25 -0800

    #9 Fix withStartup tests

--- test/enhancers/withStartup.tests.js
+++ test/enhancers/withStartup.tests.js
@@ -2,40 +2,51 @@ import { h } from 'preact';
 import { deep } from 'preact-render-spy';
 import { createSink } from 'recompose';
 import withStartup from '../../src/enhancers/withStartup';
+import getQueryString from '../../src/selectors/getQueryString';
 
 describe('enhancer withStartup', () => {
 
-	describe('with window', () => {
+	let handleQueryChange;
+	let handleQueryParamListenerChange;
+	let handleHistoryChange;
+	let handleHistoryPush;
 
-		let handleQueryChange;
-		let handleHistoryChange;
+	beforeAll(() => {
+		handleQueryChange = jest.fn();
+		handleQueryParamListenerChange = jest.fn();
+		handleHistoryChange = jest.fn();
+		handleHistoryPush = jest.fn();
 
-		beforeAll(() => {
-			handleQueryChange = jest.fn();
-			handleHistoryChange = jest.fn();
+		const Sink = withStartup(createSink(jest.fn()));
+		const props = {
+			handleQueryChange,
+			handleQueryParamListenerChange,
+			handleHistoryChange,
+			handleHistoryPush
+		};
 
-			const Sink = withStartup(createSink(jest.fn()));
-			const props = { handleQueryChange, handleHistoryChange };
+		deep(<Sink {...props} />);
 
-			deep(<Sink {...props} />);
+		global.window = {
+			location: { query: '?testing' }
+		};
+	});
 
-			global.window = {
-				location: { query: '?testing' }
-			};
-		});
+	afterAll(() => {
+		handleQueryChange = null;
+		handleHistoryChange = null;
+		global.window = null;
+	});
 
-		afterAll(() => {
-			handleQueryChange = null;
-			handleHistoryChange = null;
-			global.window = null;
-		});
+	test('should call handleQueryChange', () => {
+		expect(handleQueryChange).toHaveBeenCalledWith(getQueryString());
+	});
 
-		test('should call handleQueryChange', () => {
-			expect(handleQueryChange).toHaveBeenCalledWith(global.window.location);
-		});
+	test('should call handleQueryParamListenerChange', () => {
+		expect(handleQueryParamListenerChange).toHaveBeenCalledWith(handleHistoryPush);
+	});
 
-		test('should call handleHistoryChange', () => {
-			expect(handleHistoryChange).toHaveBeenCalledWith(handleQueryChange);
-		});
+	test('should call handleHistoryChange', () => {
+		expect(handleHistoryChange).toHaveBeenCalledWith(handleQueryChange);
 	});
 });
